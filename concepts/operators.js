ec3bfb427d8560e06e2c1c0e94713f82e0740d0c
Ared <ared230000@gmail.com>
2024-09-12 13:45:01 -0600

    02 Operadores

---
+++ concepts/operators.js
@@ -0,0 +1,35 @@
+// Operadores Mmatemáticos
+// + y -
+// * y /
+// % y ** Módulo y potencia
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 % 2);
+console.log(2 ** 2);
+
+// Operadores de asignación
+let a = 10;
+a += 5; // a = a + 5;
+a -= 5;
+a *= 5;
+a /= 5;
+a %= 5;
+a **= 5;
+console.log(a);
+
+// Operadores de comparación
+console.log(2 > 2);
+console.log(2 < 2);
+console.log(2 >= 2);
+console.log(2 <= 2);
+console.log(2 === 2);
+console.log(2 == 2);
+console.log(2 != 2);
+console.log(2 !== 2);
+
+// Operadores de lógicos
+console.log(true && true); // and
+console.log(true || false); // or
+console.log(!true); // not
